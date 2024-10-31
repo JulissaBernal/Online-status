31a12534ccad083b3ea11248939c4599fbfd933c
julissa Bernal <152914827+JulissaBernal@users.noreply.github.com>
2024-10-31 12:57:50 -0600

    Online status ready

---
+++ onlinestatus.js
@@ -0,0 +1,10 @@
+// Display online status for a list of users.
+ 
+
+// Example:
+// - `onlineStatus(['mockIng99', 'J0eyPunch', 'glassedFer'])` should return `'mockIng99, J0eyPunch and 1 more online'`.
+
+let usuario=['mockIng99', 'J0eyPunch', 'glassedFer'];
+
+let arra=usuario.slice(0,usuario.length-1);
+console.log(`${arra} and 1 more online`);
